43571c2f3e88d2eab4553426da89a5c3b9d43a39
iulia_serzantova <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>
2025-04-23 16:19:11 +0300

    fixes

--- src/main/java/nechto/controller/GameController.java
+++ src/main/java/nechto/controller/GameController.java
@@ -20,7 +20,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/game")
+@RequestMapping("/games")
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
--- src/main/java/nechto/controller/UserController.java
+++ src/main/java/nechto/controller/UserController.java
@@ -20,7 +20,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
